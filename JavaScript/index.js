8499f29cc016928346b0f0fd7e1338572e35b7b3
bas2205@live.fr <bas2205@live.fr>
2017-04-24 21:09:59 +0200

    shit done

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,34 +1,30 @@
 var timer;
-var timerValue = 10000;
+var timerValue = 100000;
 var index = 0;
 var articles;
 function SwapLeft () {
-  articles[index].removeClass("show");
-  articles[index].addClass("hide");
+  articles[index].className = "hide-left";
 
   --index;
   if (index < 0)
     index = articles.length - 1;
 
-  articles[index].removeClass("hide");
-  articles[index].addClass("show");
+  articles[index].className = "show-left";
+
   clearInterval (timer);
   timer = setInterval (SwapRight, timerValue);
 }
 
 function SwapRight () {
-  articles[index].removeClass("show");
-  articles[index].addClass("hide");
+  articles[index].className = "hide-right";
 
-  ++index;
+  index++;
   if (index == articles.length)
     index = 0;
 
-  articles[index].removeClass("hide");
-  articles[index].addClass("show");
+  articles[index].className = "show-right";
   clearInterval (timer);
   timer = setInterval (SwapRight, timerValue);
-  alert("hi");
 }
 
 $(function(){
