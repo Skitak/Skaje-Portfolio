7d7a7c1589ab43e999bd242441e096ed57bc25f0
skitak <bas2205@live.fr>
2017-04-24 10:36:17 +0000

    modifs added to carousel

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,18 +1,39 @@
 var timer;
+var timerValue = 10000;
+var index = 0;
+var articles;
+function SwapLeft () {
+  articles[index].removeClass("show");
+  articles[index].addClass("hide");
 
-var SwapLeft = function(){
-  alert("hi left");
-};
+  --index;
+  if (index < 0)
+    index = articles.length - 1;
 
-var Swapright = function(){
-  clearInterval(timer);
-  timer = setInterval (SwapRight, 10000);
+  articles[index].removeClass("hide");
+  articles[index].addClass("show");
+  clearInterval (timer);
+  timer = setInterval (SwapRight, timerValue);
+}
 
-  alert("hi right");
-};
+function SwapRight () {
+  articles[index].removeClass("show");
+  articles[index].addClass("hide");
+
+  ++index;
+  if (index == articles.length)
+    index = 0;
+
+  articles[index].removeClass("hide");
+  articles[index].addClass("show");
+  clearInterval (timer);
+  timer = setInterval (SwapRight, timerValue);
+  alert("hi");
+}
 
 $(function(){
-  $("pannel Right").click(SwapRight);
-    $("pannel Left").click(SwapLeft);
-    timer = setInterval (SwapRight, 10000);
+  articles = $(".l-body-carousel article");
+  $(".Right").on("click", SwapRight);
+    $(".Left").on("click", SwapLeft);
+    timer = setInterval (SwapRight, timerValue);
 });
