627352cff00daeb6522ad4b7597b16b98fbdefee
skitak <bas2205@live.fr>
2017-05-06 19:20:44 +0200

    mail and php added

--- Contact.php
+++ Contact.php
@@ -1,24 +1,58 @@
 <?php
   include "header.html";
+  require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+  $nom = "";
+  $email = "";
+  $tel = "";
+  $message = "";
+
+  if (!empty($_POST)){
+    $nom = !empty($_POST["nom"])? $_POST["nom"] : "";
+    $email = !empty($_POST["mail"])? $_POST["mail"] : "";
+    $tel = !empty($_POST["telephone"])? $_POST["telephone"] : "";
+    $message = !empty($_POST["message"])? $_POST["message"] : "";
+    $err = false;
+    if (empty($_POST["nom"])){
+      echo '<div class="error-name">Veuillez entrer votre nom s\'il vous plaît</div>';
+      $err = true;
+    }if (empty($_POST["mail"])){
+      echo '<div class="error-mail">Veuillez entrer votre adrese email s\'il vous plaît</div>';
+      $err = true;
+    }if (empty($_POST["message"])){
+      echo '<div class="error-mesage">Veuillez entrer un message s\'il vous plaît</div>';
+      $err = true;
+    } if  (!$err){
+      $message = wordwrap ($message, 70, "\r\n");
+      $mail = new PHPMailer;
+      $mail->setFrom($email);
+      $mail->addAddress("bas2205@live.fr");
+      $mail->Subject = "Portfolio message from : $nom";
+      $mail->Body = $message;
+      if ($mail->send())
+        echo "done";
+        else echo "error : " . $mail->ErrorInfo;
+    }
+
+  }
  ?>
-  <form class="contact" action="Contact.html" method="post">
+  <form class="contact" action="Contact.php" method="post">
     <fieldset>
       <label for="nom">Nom complet*</label>
       <input type="text" name="nom">
     </fieldset>
     <fieldset>
-    <label for="nom">Email*</label>
-    <input type="email" name="mail">
+      <label for="mail">Email*</label>
+      <input type="email" name="mail">
     </fieldset>
     <fieldset>
-    <label for="nom">Téléphone</label>
-    <input type="text" name="telephone">
+      <label for="telephone">Téléphone</label>
+      <input type="text" name="telephone">
     </fieldset>
     <fieldset>
-    <label for="nom">Message</label>
-    <textarea name="message"></textarea>
+      <label for="message">Message</label>
+      <textarea name="message"></textarea>
     </fieldset>
-    <input type="submit" value="Envoyer">
+      <input type="submit" value="Envoyer">
   </form>
 </body>
 
--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php
   include "header.html";
-
   $bdd_Address = "localhost";
   $bdd_user = "skitak";
   $bdd_password = "uic75015";
-  $bdd_name = "arthur";
+  $bdd_name = "Arthur";
 
   $connection = new mysqli($bdd_Address, $bdd_user, $bdd_password, $bdd_name);
 
-  $query = $connection->query("SELECT name,image,link FROM game");
+  $query = $connection->query("SELECT title,image,link FROM GAME");
 
  ?>
   <content class="l-body-carousel">
@@ -16,13 +15,11 @@
     <div class="pannel Right"><span class="arrow"></span></div>
     <?php
 
-      for ($rowNum = $query->num_rows -1 ; $rowNum >= 0; $rowNUm--){
-        $query->data_seek($row_no);
-        $row = $res->fetch_assoc();?>
+      while ($row = $query->fetch_assoc()){?>
 
         <a href="#" class="hidden">
           <article >
-            <img src=<?php echo '"' . $row['image'] . '"';?> alt="Bjr">
+            <img src=<?php echo '"Images/' . $row['image'] . '"';?> alt="Bjr">
             <h2>Nom de jeu</h2>
           </article>
         </a>
