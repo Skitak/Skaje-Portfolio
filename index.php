5a40def8568cd4515570ffefe47819ab3e080264
skitak <bas2205@live.fr>
2017-05-11 10:32:28 +0000

    modifs index of elements and first entry

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
   if (!session_id ())
     session_start();
   include "header.html";
-  
+
   $_SESSION["bdd"] = array();
   $_SESSION["hi"] = "hi";
   $_SESSION["bdd"]["Address"] = "sql11.freesqldatabase.com";
@@ -15,15 +15,23 @@
                            $_SESSION["bdd"]["password"],
                            $_SESSION["bdd"]["name"]);
 
-  $query = $connection->query("SELECT id, image, title FROM GAME");
+  $query = $connection->query("SELECT id, image, title FROM GAME ORDER BY id desc");
 
  ?>
   <content class="l-body-carousel">
     <div class="pannel Left"><span class="arrow"></span></div>
     <div class="pannel Right"><span class="arrow"></span></div>
     <?php
-      while ($row = $query->fetch_assoc()){?>
-        <a href=<?php echo '"Jeu.php?id=' . $row['id'] . '"';?> class="hidden">
+    $number = 0;
+    $class;
+      while ($row = $query->fetch_assoc()){
+        if ($number == 0)
+          $class = "show-right";
+        else if ($numer == 1)
+          $class = "fading";
+        else $class = "hidden";
+          $number++?>
+        <a href=<?php echo '"Jeu.php?id=' . $row['id'] . '"';?> class=<?php echo '"' . $class . '"';?>>
           <article >
             <img src=<?php echo '"Images/' . $row['image'] . '"';?> alt="Bjr">
             <h2><?php echo $row['title'];?></h2>
