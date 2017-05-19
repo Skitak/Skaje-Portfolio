bac60458a23efd7492d982c64c123e6814463ab6
skitak <bas2205@live.fr>
2017-05-19 12:53:56 +0200

    cv added

--- compte.php
+++ compte.php
@@ -6,21 +6,17 @@ if (!isset($_SESSION["connected"]) || !$_SESSION["connected"]){
   header("Location:http://arthursorignet.com");
   exit();
 }
+
 $connection = new mysqli("sql11.freesqldatabase.com", "sql11173301", "AbDUiMCajz", "sql11173301");
-if (!empty($_POST)){
-  if(!file_exists($_FILES['fileToUpload']['tmp_name']) || !is_uploaded_file($_FILES['fileToUpload']['tmp_name'])){
-    $query = $connection->query("UPDATE ACCOUNT SET mail='" . $_POST["mail"] . "',text='"
-                            . $_POST["text"] . "'");
-    header("Location:http://arthursorignet.com/menu.php");
-    exit();
-  } else {
-  $query = $connection->query("SELECT image FROM ACCOUNT")->fetch_assoc();
+
+function uploadFile($nameOfFile, $connection){
+  $query = $connection->query("SELECT $nameOfFile FROM ACCOUNT")->fetch_assoc();
   $target_dir = "Assets/";
-  $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+  $target_file = $target_dir . basename($_FILES[$nameOfFile]["name"]);
   $uploadOk = 1;
   $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
   if(isset($_POST["submit"])) {
-      $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+      $check = getimagesize($_FILES[$nameOfFile]["tmp_name"]);
       if($check !== false) {
           echo "File is an image - " . $check["mime"] . ".";
           $uploadOk = 1;
@@ -29,28 +25,31 @@ if (!empty($_POST)){
           $uploadOk = 0;
       }
   }
-  if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-  && $imageFileType != "gif" ) {
-      echo "format jgp, jpeg, png ou gif stp";
-      $uploadOk = 0;
-  }
   if ($uploadOk == 0) {
     echo "il y a eu un problème du coup gros";
   } else {
-      if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        unlink($target_dir . $query["image"]);
-        $query = $connection->query("UPDATE ACCOUNT SET mail='" . $_POST["mail"] . "',text='". $_POST["text"] . "'" . ",image='" .
-                                       $_FILES["fileToUpload"]["name"] .
-                                      "'");
-        header("Location:http://arthursorignet.com/menu.php");
-        exit();
+      if (move_uploaded_file($_FILES[$nameOfFile]["tmp_name"], $target_file)) {
+        unlink($target_dir . $query[$nameOfFile]);
+        $query = $connection->query("UPDATE ACCOUNT SET $nameOfFile='" . $_FILES[$nameOfFile]["name"] . "'");
       } else {
         echo "Ha merde, il y a eu un problème ... Envoie un sms et j'essaie de règler ça (j'ai dû reçevoir un mail normalement)";
         mail("bas2205@live.fr","pb site Arthur", "Faut que tu check le site de Arthur, il a pas pu upload.");
       }
   }
-
 }
+if (!empty($_POST)){
+  if(!(!file_exists($_FILES['image']['tmp_name']) || !is_uploaded_file($_FILES['image']['tmp_name']))){
+    uploadFile("image", $connection);
+  }
+
+  if(!(!file_exists($_FILES['cv']['tmp_name']) || !is_uploaded_file($_FILES['cv']['tmp_name']))){
+    uploadFile("cv", $connection);
+  }
+
+    $query = $connection->query("UPDATE ACCOUNT SET mail='" . $_POST["mail"] . "',text='"
+                            . $_POST["text"] . "'");
+    header("Location:http://arthursorignet.com/menu.php");
+    exit();
 }
 else {
   $query = $connection->query("SELECT mail, text FROM ACCOUNT");
@@ -69,8 +68,12 @@ else {
       <textarea name="text"><?php echo $row['text'];?></textarea>
     </fieldset>
     <fieldset>
-      <label for="fileToUpload">Photo</label>
-      <input type="file" name="fileToUpload">
+      <label for="image">Photo</label>
+      <input type="file" name="image">
+    </fieldset>
+    <fieldset>
+      <label for="cv">CV</label>
+      <input type="file" name="cv">
     </fieldset>
       <input type="submit" value="submit">
   </form>
